ecc77ab645f4f9cf9303ffe8723714b2c75d58cc
Justin Moore <jusmoore_github@jublin.xyz>
2026-08-02 10:54:35 -0700

    feat(ui): add tailing workspace and search views

--- src/HexTailSharp/Program.cs
+++ src/HexTailSharp/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using HexTailSharp;
+using HexTailSharp.Application;
+using HexTailSharp.Persistence;
+using HexTailSharp.Tailing;
 using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,5 +12,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddRadzenComponents();
+builder.Services.AddScoped<TailerService>();
+builder.Services.AddScoped<IAppPersistence, BrowserLocalStoragePersistence>();
+builder.Services.AddScoped<AppState>();
 
 await builder.Build().RunAsync();
